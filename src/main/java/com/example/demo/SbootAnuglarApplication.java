6bb692e2449b26991a552041cd7b0d7a81ae41cf
sreenivas <sreenivas.kuruva@gmail.com>
2018-12-24 22:07:13 +0530

    pipeline trigger

--- src/main/java/com/example/demo/SbootAnuglarApplication.java
+++ src/main/java/com/example/demo/SbootAnuglarApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SbootAnuglarApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Spring boot::::::::::::");
 		SpringApplication.run(SbootAnuglarApplication.class, args);
 	}
 }
